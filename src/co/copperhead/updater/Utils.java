13bb2a7e23f703e570a2792a8e4d7e93736686ab
Zipsnet <hcolmenares@gmail.com>
2018-04-20 12:11:47 -0400

    Updater: AICPify!
    Change-Id: Icb61b74289eee3670c306dc13669ce6e36910a95

---
+++ src/co/copperhead/updater/Utils.java
@@ -0,0 +1,36 @@
+package co.copperhead.Utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Utils {
+
+    public static String getDate() {
+        return new SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(new Date(System
+                .currentTimeMillis()));
+    }
+
+    public static String getProp(String prop) {
+        try {
+            Process process = Runtime.getRuntime().exec("getprop " + prop);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
+            StringBuilder log = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+            return log.toString();
+        } catch (IOException e) {
+            // Runtime error
+        }
+        return null;
+    }
+
+}
